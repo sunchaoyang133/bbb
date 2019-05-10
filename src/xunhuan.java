87606523fd0178b55778cf1c1572a650d0af7e94
scy <45815687@qq.com>
2019-05-10 19:23:53 +0800

    掩饰一次

---
+++ src/xunhuan.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class xunhuan {
+	public static void main(String[]args){
+		Scanner th=new Scanner(System.in);
+		System.out.println("请输入距离");
+		int  juli=th.nextInt();
+		if(juli>5){
+			while(true){
+		
+				System.out.println("你是个大傻子");
+			     break;
+			}
+		}
+		
+		
+	}
+
+}
